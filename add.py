447d6bc01e8f8c8ac091887574f8bbdab262d61f
Sonu Kumar Singh <hi.ersonusingh@gmail.com>
2023-10-03 15:28:31 +0530

    .

---
+++ add.py
@@ -0,0 +1,9 @@
+number = input("Enter the number to check : ")
+if(number>0):
+    print("Number is Positive")
+elif(number < 0):
+    print("Number is negative")
+elif(number ==0):
+    print("Number is zero")
+else:
+    print("Try again")
